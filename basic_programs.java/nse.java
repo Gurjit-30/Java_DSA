b6c023223c452eedf5c8e3a68b0393a8efebb83c
Gurjit-30 <gurjitsingh3028@gmail.com>
2025-06-11 18:38:18 +0530

    new file:   basic_programs.java/Codechef.class

---
+++ basic_programs.java/nse.java
@@ -0,0 +1,23 @@
+class nse
+{
+	public static void main (String[] args)
+	{
+		int a = 15;
+        
+        if (a % 7 == 0 && a % 5 == 0) {
+            System.out.println("The number is divisible by both 5 & 7");
+        }
+        else {
+            System.out.println("The number is not divisible by both 5 & 7");
+        }
+        
+        a = 35;
+
+        if (a % 7 == 0 && a % 5 == 0) {
+            System.out.println("The number is divisible by both 5 & 7");
+        }
+        else {
+            System.out.println("The number is not divisible by both 5 & 7");
+        }
+	}
+}
