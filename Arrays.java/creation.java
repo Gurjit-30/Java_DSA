7390f2b3c443de8ad4f694a10486cc382bc99513
Gurjit-30 <gurjitsingh3028@gmail.com>
2025-06-22 18:53:36 +0530

    new file:   Arrays.java/subarrays

---
+++ Arrays.java/creation.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+
+
+public class creation {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr=new int[50];
+        int[] drr={1,3,5,6,7};
+        // for input 
+        for(int i=0;i<=5;i++){
+            arr[i]=sc.nextInt();
+
+        }
+         for(int i=0;i<=5;i++){
+            System.out.println(arr[i]);
+
+        }
+
+    }
+    
+}
